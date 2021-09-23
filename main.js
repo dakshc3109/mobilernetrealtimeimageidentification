3bfc435730f3ee25bb6f4d47fd2a0118ca6afdc7
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-09-23 17:22:36 +0530

    completed project

--- main.js
+++ main.js
@@ -2,4 +2,26 @@ function setup() {
   canvas = createCanvas(200, 200);
   video = createCapture(VIDEO);
   video.hide();
+  
+  classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("mode loaded");
+}
+
+function draw(){
+  image(video, 0, 0, 200, 200);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_onject_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
